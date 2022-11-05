6b617b3133224cc35712caba195f21c995100018
Damian Cyntler <damian@cyntler.com>
2022-11-05 20:57:58 +0100

    feat: move default data to template root

---
+++ template/data.tsx
@@ -0,0 +1,16 @@
+export default {
+  name: 'library-demo',
+  githubLink: 'https://github.com/cyntler/library-demo',
+  examples: [
+    {
+      label: 'Placeholder 1',
+      description: 'Place for your component or element.',
+      element: <div>Test</div>,
+    },
+    {
+      label: 'Placeholder 2',
+      description: 'Place for your component or element.',
+      element: <div>Test 2</div>,
+    },
+  ],
+};
