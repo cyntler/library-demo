e26ea302dadd0b42bd5543936bf7580fd2ea07a0
Damian Cyntler <damian@cyntler.com>
2022-11-05 20:58:24 +0100

    feat: add vite-plugin-html

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+import data from './data';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data,
+      },
+    }),
+  ],
 });
