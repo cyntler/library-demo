def1471fdfd6a21f3cdf34b827d224b86e6df8dd
Damian Cyntler <damian@cyntler.com>
2022-11-06 14:04:01 +0100

    feat: update template config

---
+++ template/filesToRootMove/DO_NOT_EDIT_TEMPLATE.md
@@ -0,0 +1,1 @@
+In the `__template` catalog is an application with a ready-made demo template. Each rerun of the demo-template initialization command may overwrite the changes in this directory.
--- template/vite.config.ts
+++ template/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
-import data from './data';
+import data from './dataExport';
 
 export default defineConfig({
   plugins: [
@@ -10,7 +10,7 @@ export default defineConfig({
     createHtmlPlugin({
       minify: true,
       inject: {
-        data,
+        data: data(),
       },
     }),
   ],
