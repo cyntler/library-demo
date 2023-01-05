6fdc234e58832f494b87404864f6a47dc367d9b4
Damian Cyntler <damian@cyntler.com>
2023-01-05 21:41:05 +0100

    feat: remove packages when uninstalling

--- src/processes/uninstall.ts
+++ src/processes/uninstall.ts
@@ -1,16 +1,20 @@
+import { execSync } from 'child_process';
 import { existsSync, rmSync } from 'fs';
 import { consumerDemoScripts } from '../models';
 import { getConsumerProjectPaths } from '../utils/getConsumerProjectPaths';
 import { logError, logProgress, logSuccess } from '../utils/logger';
 import { removeScriptsFromPackageJson } from '../utils/packageJsonManipulate';
 
-const { projectDemoPath } = getConsumerProjectPaths();
+const { projectDemoPath, projectRootPath } = getConsumerProjectPaths();
 
 export const uninstall = () => {
   if (existsSync(projectDemoPath)) {
     logProgress('Demo uninstalling...');
 
     rmSync(projectDemoPath, { recursive: true, force: true });
+    execSync('npm rm @cyntler/library-demo@latest gh-pages', {
+      cwd: projectRootPath,
+    });
     removeScriptsFromPackageJson(consumerDemoScripts);
 
     logSuccess('Demo has been uninstalled!');
